68d88e42e47bdea08d9d294857df8c14a997a906
Kodanne-bakeli <kodanne.bakeli@gmail.com>
2023-08-07 12:56:52 +0000

    integration feedbacks"

--- firebase.js
+++ firebase.js
@@ -1,11 +1,10 @@
-// Import the functions you need from the SDKs you need
+// Importons les fonctions dont on a besoin à partir des SDK dont on a besoin
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
+// TODO: Ajoutons des SDK pour les produits Firebase qu'on souhaite utiliser
 // https://firebase.google.com/docs/web/setup#available-libraries
 import {
   getFirestore,
   collection,
-  getDocs,
   onSnapshot,
   addDoc,
   deleteDoc,
@@ -14,37 +13,48 @@ import {
   updateDoc,
 } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-firestore.js";
 
-// Your web app's Firebase configuration
+// La configuration Firebase de notre application Web
 const firebaseConfig = {
-    
-        apiKey: "AIzaSyCF0_Sn4Rwh4d4zY7-RcDgWjIJYi94MEoU",
-        authDomain: "crud-with-firebase-4b261.firebaseapp.com",
-        databaseURL: "https://crud-with-firebase-4b261-default-rtdb.asia-southeast1.firebasedatabase.app",
-        projectId: "crud-with-firebase-4b261",
-        storageBucket: "crud-with-firebase-4b261.appspot.com",
-        messagingSenderId: "401646212235",
-        appId: "1:401646212235:web:5162a3bb326516cb32708e"
-    
+
+  apiKey: "AIzaSyCF0_Sn4Rwh4d4zY7-RcDgWjIJYi94MEoU",
+  authDomain: "crud-with-firebase-4b261.firebaseapp.com",
+  databaseURL: "https://crud-with-firebase-4b261-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "crud-with-firebase-4b261",
+  storageBucket: "crud-with-firebase-4b261.appspot.com",
+  messagingSenderId: "401646212235",
+  appId: "1:401646212235:web:5162a3bb326516cb32708e"
+
 };
 
-// Initialize Firebase
+// Initialiser firebase
 export const app = initializeApp(firebaseConfig);
 
-export const database = getFirestore();
+//initialiser le cloud firestore et obtenir une référence au service
+export const database = getFirestore(app);
+//exportons les fonctions pour notre crud
 
-
+//fonction pour ajouter des données dans la database
 export const addTask = (name, email, message) =>
   addDoc(collection(database, "users"), { name, email, message });
 
+//fonction pour lister tous les documents
 export const onGetTasks = (callback) =>
   onSnapshot(collection(database, "users"), callback);
 
-
+//fonction pour supprimer des documents
 export const deleteTask = (id) => deleteDoc(doc(database, "users", id));
 
+//fonction pour lister un document
 export const getTask = (id) => getDoc(doc(database, "users", id));
 
+//fonction pour mettre à jour les documents
 export const updateTask = (id, newFields) =>
   updateDoc(doc(database, "users", id), newFields);
 
-export const getTasks = () => getDocs(collection(database, "users"));
+export const crud ={
+  addTask:(name, email, message) =>
+  addDoc(collection(database, "users"), { name, email, message }),
+  onGetTasks : (callback) =>
+  onSnapshot(collection(database, "users"), callback)
+}
+
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+// Importons les fonctions dont on a besoin 
 import {
   onGetTasks,
   addTask,
   deleteTask,
   getTask,
   updateTask,
-  getTasks,
+  crud
 } from "./firebase.js";
 
-
+crud.onGetTasks
 
 //Variables Global
 let name = document.getElementById('name')
@@ -16,25 +17,27 @@ let message = document.getElementById('message')
 let add = document.querySelector('#card')
 let submit = document.querySelector('.btn')
 let form = document.querySelector(".add-post")
+let loader = document.getElementById("loader")
+let span = document.getElementById("spanarret")
 
 
-
+//désactivation du status de mise a jour
 let editStatus = false;
 let id = "";
-
+//ecoutons l'evenement pour que tout soit chargé avant d'être afficher
 window.addEventListener("DOMContentLoaded", async (e) => {
 
-
+  //écoutons tous les documents de la collection
   onGetTasks((querySnapshot) => {
     add.innerHTML = "";
 
     querySnapshot.forEach((doc) => {
       const user = doc.data();
-
+      //rendu des données
       add.innerHTML += `
       <div class="card card-body mt-2 border-primary">
-    <h3 class="h5">${user.name}</h3>
-    <h3 class="h5">${user.email}</h3>
+    <h3 class="card-title card-title">${user.name}</h3>
+    <h4 class="card-title">${user.email}</h4>
     <p>${user.message}</p>
     <div>
       <button class="btn btn-primary delete" data-id="${doc.id}">
@@ -46,31 +49,38 @@ window.addEventListener("DOMContentLoaded", async (e) => {
     </div>
   </div>`;
     });
-// FONCTION POUR SUPPRIMER
-
+    // FONCTION POUR SUPPRIMER
+    //selectionnons tout les bouttons
     const btnsDelete = add.querySelectorAll(".delete");
+    //pour chaque bouttons...
     btnsDelete.forEach((btn) =>
+      //écoutons l'evenement
       btn.addEventListener("click", async ({ target: { dataset } }) => {
         try {
           await deleteTask(dataset.id);
         } catch (error) {
           console.log(error);
         }
+        
+       
       })
     );
 
-// FONCTION POUR METTRE A JOUR
-
+    // FONCTION POUR METTRE A JOUR
+    //selectionnons tout les bouttons
     const btnsEdit = add.querySelectorAll(".edit");
+    //pour chaque bouttons...
     btnsEdit.forEach((btn) => {
+      //écoutons l'evenement
       btn.addEventListener("click", async (e) => {
+        //éxécutons les intructions
         try {
           const doc = await getTask(e.target.dataset.id);
           const user = doc.data();
           name.value = user.name;
           email.value = user.email;
           message.value = user.message;
-          
+
           editStatus = true;
           id = doc.id;
           submit.innerText = "Update";
@@ -83,17 +93,34 @@ window.addEventListener("DOMContentLoaded", async (e) => {
 });
 
 // FONCTION POUR AJOUTER
-
+//écoutons l'evenement...
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
 
   const namel = name;
   const emaill = email;
   const messagel = message;
-
+//éxécutons les intructions
   try {
     if (!editStatus) {
-      await addTask(namel.value, emaill.value, messagel.value);
+ 
+  if(namel.value==="" && emaill.value==="" && messagel.value===""){
+    alert("veuillez entrer des données")
+  }else{ 
+  //        submit.addEventListener("click", () => {
+  //    loader.classList.remove("hidden")
+  //    span.classList.add("hidden")
+  //    submit.setAttribute("disabled", "")
+  //    setTimeout(() => {
+  //      loader.classList.add("hidden")
+  //      span.classList.remove("hidden")
+  //      submit.removeAttribute("disabled")
+  //    }, 3000);
+  //  })
+    await addTask(namel.value, emaill.value, messagel.value);
+  }
+      
+      
     } else {
       await updateTask(id, {
         name: namel.value,
